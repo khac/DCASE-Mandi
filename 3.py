b3e2dc429d1f8635c99dd1372f2edb6efba781a3
Adit Chawdhary <khac@users.noreply.github.com>
2017-09-28 17:14:23 +0530

    Update 3.py

--- 3.py
+++ 3.py
@@ -9,28 +9,11 @@ import numpy as np
 import _pickle as cPickle
 
 print('Load task definition')
-'''
-with open('/home/adit/Desktop/DCASE2017-baseline-system-master/Text_DCASE/ubm_list.txt') as inputFile:#'/home/deepu/research/DCASE2017/Data/ubm_list.txt') as inputFile:
-    ubmList = inputFile.read().split('\n')
-
-
-nameList = []
-for a in ubmList:
-    nameList.append(a.rsplit("/")[-1])
-#directory = os.fsencode("/home/deepu/research/DCASE2017/Data/EmoDB_MFEC_HDF5")#"/home/adit/Desktop")# )
-#os.chdir = "/home/adit/Desktop/DCASE2017-baseline-system-master/applications/data/TUT-acoustic-scenes-2017-development/audio/"
-'''
 with open('/home/adit/Desktop/DCASE2017-baseline-system-master/Text_DCASE/fold1_train_names.txt') as inputFile:#'/home/deepu/research/DCASE2017/Data/ubm_list.txt') as inputFile:
     ubmList = inputFile.read().split('\n')
 nameList = []
 for a in ubmList:
     nameList.append(a.rsplit(".")[0])
-
-
-#for file in os.listdir(directory):
-#	filename = os.fsdecode(file)
-	#if filename.endswith(".wav"): 
-	#print(os.path.join(directory, filename))
 features_server = sidekit.FeaturesServer(features_extractor=None,
                                          feature_filename_structure="../HDF5_DCASE/Features/{}.h5",
                                          sources=None,
@@ -49,8 +32,6 @@ features_server = sidekit.FeaturesServer(features_extractor=None,
                                          traps_dct_nb=None,
                                          rasta=False,
                                          keep_all_features=None)
-
-#for i in range(0,11):	
 print('Train the UBM by EM')
 # Extract all features and train a GMM without writing to disk
 distribNb = 512#np.power(2,i)
@@ -63,13 +44,3 @@ print('__2__Saving for '+str(distribNb))
 ubm.write(p+'/ubm_'+str(distribNb)+'.h5')
 cPickle.dump( ubm, open( p+"models/ubm_"+str(distribNb)+".p", "wb" ) )
 cPickle.dump( llk, open( p+"models/llk_"+str(distribNb)+".p", "wb" ) )
-
-#ubm.write('/ubm_{}.h5'.format(distribNb))
-#targetModelFile = '../Model/ubm'+str(distribNb)+'.h5'
-#ubm.write(targetModelFile)
-
-#enroll_idmap = sidekit.IdMap('/home/adit/Desktop/DCASE2017-baseline-system-master/451/ubm'+str(distribNb)+'.h5')
-#es = sidekit.StatServer(distrib_nb=distribNb, feature_size=40)
-#regulation_factor = 16  # MAP regulation factor
-#enroll_sv = es.adapt_mean_map(ubm, regulation_factor)
-#enroll_sv.write('/home/adit/Desktop/DCASE2017-baseline-system-master/451/adapted_gmm'+str(distribNb)+'.h5')
