0ead7aec30948096402eaa8e0f1456459d05a61e
Adit Chawdhary <khac@users.noreply.github.com>
2017-09-28 17:11:10 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -7,14 +7,14 @@ import h5py
 import numpy as np
 import re
 
-inpDirPath ='/home/adit/Desktop/DCASE2017-baseline-system-master/applications/data/TUT-acoustic-scenes-2017-development/audio'# '/home/deepu/research/DCASE2017/Data/EmoDB/535/'
+inpDirPath ='/home/adit/Desktop/DCASE2017-baseline-system-master/applications/data/TUT-acoustic-scenes-2017-development/audio'
 ubmList,nameList = [],[]
 for rootDirPath, subDir, files in os.walk(inpDirPath):
    for fileName in files:
        relFile = os.path.join(rootDirPath, fileName)
        ubmList.append(os.path.splitext(relFile)[0])
        nameList.append(os.path.splitext(relFile)[0].rsplit("/")[-1])
-       #print(os.path.splitext(relFile)[0])
+       
 with open('../Text_DCASE/ubm_list.txt','w') as of:#'../Data/ubm_list.txt','w') as of:
    of.write("\n".join(ubmList))
 with open('../Text_DCASE/name_list.txt','w') as of:
