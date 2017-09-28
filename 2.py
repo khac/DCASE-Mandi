377f61499eaa8686b3afe3db401cd821e2a443d6
Adit Chawdhary <khac@users.noreply.github.com>
2017-09-28 17:13:12 +0530

    Update 2.py

--- 2.py
+++ 2.py
@@ -9,30 +9,18 @@ import numpy as np
 
 
 print('Load task definition')
-with open('/home/adit/Desktop/DCASE2017-baseline-system-master/Text_DCASE/fold1_train_names.txt') as inputFile:#ubm_list.txt') #'/home/deepu/research/DCASE2017/Data/ubm_list.txt') as inputFile:
+with open('/home/adit/Desktop/DCASE2017-baseline-system-master/Text_DCASE/fold1_train_names.txt') as inputFile: 
     ubmList = inputFile.read().split('\n')
-'''
-with open('/home/adit/Desktop/DCASE2017-baseline-system-master/Text_DCASE/name_list.txt') as inputFile:#'/home/deepu/research/DCASE2017/Data/ubm_list.txt') as inputFile:
-    nameList = inputFile.read().split('\n')
-'''
 nameList = []
 for a in ubmList:
     nameList.append(a.rsplit('.')[0])
 
-directory = '/home/adit/Desktop/DCASE2017-baseline-system-master/applications/data/TUT-acoustic-scenes-2017-development/audio/'#'/home/deepu/research/DCASE2017/Data/EmoDB/535'
-#"/home/adit/Desktop")# )
-#os.chdir("/home/adit/Desktop/DCASE2017-baseline-system-master/applications/data/TUT-acoustic-scenes-2017-development/audio/")
-
-#for file in os.listdir(directory):
-#	filename = os.fsdecode(file)
-#	if filename.endswith(".wav"): 
-	#print(os.path.join(directory, filename))
-		
+directory = '/home/adit/Desktop/DCASE2017-baseline-system-master/applications/data/TUT-acoustic-scenes-2017-development/audio/'
 extractor = sidekit.FeaturesExtractor(audio_filename_structure = "/home/adit/Desktop/DCASE2017-baseline-system-master/applications/data/TUT-acoustic-scenes-2017-development/audio/{}.wav",
 					feature_filename_structure = "/home/adit/Desktop/shikha_maam/Features/{}.h5",
-					sampling_frequency  =   16000,#44100,
+					sampling_frequency  =   16000,
                                         lower_frequency     =   10,
-                                        higher_frequency    =   8000,#22050,
+                                        higher_frequency    =   8000,
                                         filter_bank         =   "log",
                                         filter_bank_size    =   40,
                                         window_size         =   0.040,
