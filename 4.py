efc49e840d14f17df14f8eeb00ab9b91b0d619e4
Adit Chawdhary <khac@users.noreply.github.com>
2017-09-28 17:22:03 +0530

    Update 4.py

--- 4.py
+++ 4.py
@@ -8,17 +8,10 @@ import logging
 import numpy as np
 
 directory = os.fsencode("/home/adit/Desktop/DCASE2017-baseline-system-master/Model_DCASE")#"/home/adit/Desktop")
-
-'''
-for file in os.listdir(directory):
-	filename = os.fsdecode(file)
-	distribNb = int(filename.rsplit("_")[-1][0])
-	#enroll_idmap = sidekit.IdMap('../Model_DCASE/'+filename)
-'''
 distribNb = 2048
 ubm = sidekit.Mixture()
 enroll_stat = sidekit.StatServer(distrib_nb=distribNb, feature_size=40)
 regulation_factor = 3  # MAP regulation factor
 enroll_sv = enroll_stat.adapt_mean_map(ubm, regulation_factor)
-enroll_sv.write('gmm_adapted_12345.h5')
+enroll_sv.write('gmm_adapted.h5')
 
